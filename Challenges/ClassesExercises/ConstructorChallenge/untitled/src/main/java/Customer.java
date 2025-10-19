2a286a6fe0b6a720c9c576ab3563fae7e196dff9
Ibrahim Banat <i.banat@syarah.com>
2025-10-19 17:04:07 +0300

    constructor chaining and overloading

---
+++ Challenges/ClassesExercises/ConstructorChallenge/untitled/src/main/java/Customer.java
@@ -0,0 +1,44 @@
+public class Customer {
+
+    private String name;
+    private double creditLimit;
+    private String email;
+
+    public Customer(String name, double creditLimit, String email) {
+        this.name = name;
+        this.creditLimit = creditLimit;
+        this.email = email;
+    }
+
+    public Customer(String name, String email) {
+        this(name, 0, email);
+    }
+
+    public Customer() {
+        this("Nobody", "nobody@default.com");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getCreditLimit() {
+        return creditLimit;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCreditLimit(double creditLimit) {
+        this.creditLimit = creditLimit;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
